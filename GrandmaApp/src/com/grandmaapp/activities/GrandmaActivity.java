3c3d65eb8c56c417d2001ca8d46de4928835663f
Severin <schnuette@googlemail.com>
2013-12-17 13:17:33 +0100

    Service für Wünsche usw.

--- GrandmaApp/src/com/grandmaapp/activities/GrandmaActivity.java
+++ GrandmaApp/src/com/grandmaapp/activities/GrandmaActivity.java
@@ -1,17 +1,15 @@
 package com.grandmaapp.activities;
 
-import com.example.grandmaapp.R;
-import com.example.grandmaapp.R.drawable;
-import com.example.grandmaapp.R.id;
-import com.example.grandmaapp.R.layout;
-import com.example.grandmaapp.R.menu;
+import java.util.Calendar;
 
-import android.os.Build;
-import android.os.Bundle;
-import android.app.ActionBar;
 import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.os.Bundle;
 import android.util.DisplayMetrics;
-import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.view.WindowManager;
@@ -19,7 +17,9 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.LinearLayout.LayoutParams;
-import android.widget.TextView;
+
+import com.example.grandmaapp.R;
+import com.grandmaapp.services.WishesService;
 
 public class GrandmaActivity extends Activity {
 
@@ -27,11 +27,12 @@ public class GrandmaActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_grandma);
-		
+
+
+		startWishesService();
 		adjustGUI();
-		
+
 	}
-	
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -39,61 +40,82 @@ public class GrandmaActivity extends Activity {
 		getMenuInflater().inflate(R.menu.grandma, menu);
 		return true;
 	}
-	
-	public void zeigeEinstellungen(View view){
-		 
+
+	public void zeigeEinstellungen(View view) {
+
 	}
-	
-	private void adjustGUI(){
+
+	private void adjustGUI() {
 		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-		        WindowManager.LayoutParams.FLAG_FULLSCREEN);
-		
+				WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
 		DisplayMetrics displaymetrics = new DisplayMetrics();
 		getWindowManager().getDefaultDisplay().getMetrics(displaymetrics);
 		int height = displaymetrics.heightPixels / 2;
 		int width = displaymetrics.widthPixels / 3;
 		ImageView grandmaImgV = (ImageView) findViewById(R.id.grandmaImgView);
 		grandmaImgV.getLayoutParams().height = height;
-		
+
 		grandmaImgV.setImageResource(R.drawable.grandma_zufrieden);
-		
+
 		Button settingsBtn = (Button) findViewById(R.id.einstellungenBtn);
 		Button supplyBtn = (Button) findViewById(R.id.vorraeteBtn);
 		Button testBtn = (Button) findViewById(R.id.testBtn);
-		
+
 		settingsBtn.setBackgroundResource(R.drawable.settings_selector);
 		supplyBtn.setBackgroundResource(R.drawable.supply_selector);
 		testBtn.setBackgroundResource(R.drawable.test_selector);
-		
-		settingsBtn.getLayoutParams().width = (width - (width/10));
-		supplyBtn.getLayoutParams().width = (width - (width/10));
-		testBtn.getLayoutParams().width = (width - (width/10));
-		
+
+		settingsBtn.getLayoutParams().width = (width - (width / 10));
+		supplyBtn.getLayoutParams().width = (width - (width / 10));
+		testBtn.getLayoutParams().width = (width - (width / 10));
+
 		LinearLayout linLay = (LinearLayout) findViewById(R.id.tasksLinLay);
 		Button txt1 = new Button(this);
 		txt1.setText("hallo hallo");
 		txt1.setBackgroundResource(R.drawable.button_selector);
-		txt1.setLayoutParams((new LayoutParams(LayoutParams.MATCH_PARENT,LayoutParams.WRAP_CONTENT)));
-        linLay.addView(txt1);
-        
-        Button txt2 = new Button(this);
-        txt2.setText("hallo hallo");
-        txt2.setBackgroundResource(R.drawable.button_selector);
-        txt2.setLayoutParams((new LayoutParams(LayoutParams.MATCH_PARENT,LayoutParams.WRAP_CONTENT)));
-        linLay.addView(txt2);
-        
-        Button txt3 = new Button(this);
-        txt3.setText("hallo hallo");
-        txt3.setBackgroundResource(R.drawable.button_selector);
-        txt3.setLayoutParams((new LayoutParams(LayoutParams.MATCH_PARENT,LayoutParams.WRAP_CONTENT)));
-        linLay.addView(txt3);
-        
-        Button txt4 = new Button(this);
-        txt4.setText("hallo hallo");
-        txt4.setBackgroundResource(R.drawable.button_selector);
-        txt4.setLayoutParams((new LayoutParams(LayoutParams.MATCH_PARENT,LayoutParams.WRAP_CONTENT)));
-        linLay.addView(txt4);
+		txt1.setLayoutParams((new LayoutParams(LayoutParams.MATCH_PARENT,
+				LayoutParams.WRAP_CONTENT)));
+		Typeface font = Typeface.createFromAsset(getAssets(), "Blippo.ttf");  
+		txt1.setTypeface(font);  
+		txt1.setTextSize(30);
+		linLay.addView(txt1);
+
+		Button txt2 = new Button(this);
+		txt2.setText("hallo hallo");
+		txt2.setBackgroundResource(R.drawable.button_selector);
+		txt2.setLayoutParams((new LayoutParams(LayoutParams.MATCH_PARENT,
+				LayoutParams.WRAP_CONTENT)));
+		linLay.addView(txt2);
+
+		Button txt3 = new Button(this);
+		txt3.setText("hallo hallo");
+		txt3.setBackgroundResource(R.drawable.button_selector);
+		txt3.setLayoutParams((new LayoutParams(LayoutParams.MATCH_PARENT,
+				LayoutParams.WRAP_CONTENT)));
+		linLay.addView(txt3);
+
+		Button txt4 = new Button(this);
+		txt4.setText("hallo hallo");
+		txt4.setBackgroundResource(R.drawable.button_selector);
+		txt4.setLayoutParams((new LayoutParams(LayoutParams.MATCH_PARENT,
+				LayoutParams.WRAP_CONTENT)));
+		linLay.addView(txt4);
+
+	}
+
+	private void startWishesService() {
+		//Sekundentakt in dem der Service gestartet wird.
+		int refreshInS = 60;
 		
+		Calendar cal = Calendar.getInstance();
+
+		Intent intent = new Intent(this, WishesService.class);
+		PendingIntent pintent = PendingIntent.getService(this, 0, intent, 0);
+
+		AlarmManager alarm = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+		alarm.setRepeating(AlarmManager.RTC_WAKEUP, cal.getTimeInMillis(),
+				refreshInS * 1000, pintent);
 	}
 
 }
---
+++ GrandmaApp/src/com/grandmaapp/services/WishesService.java
@@ -0,0 +1,32 @@
+package com.grandmaapp.services;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.text.format.Time;
+import android.util.Log;
+
+
+public class WishesService extends Service {
+	
+	@Override
+	public int onStartCommand(Intent intent, int flags, int startId) {
+		// TODO Wuensche generieren, Benutzer informieren usw
+		
+		Time now = new Time();
+		now.setToNow();
+		
+		//time as integer from 0 to 2359 (hour and minutes)
+		int time = Integer.parseInt(now.format2445().substring(9, 13));
+		Log.d("test", String.valueOf(time));
+		
+		return Service.START_STICKY;
+	}
+
+	@Override
+	public IBinder onBind(Intent arg0) {
+		// TODO for communication return IBinder implementation
+		return null;
+	}
+
+}
