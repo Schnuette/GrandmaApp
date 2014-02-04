a13ad0417df9e96a411507da614b142fae1db969
Tharlos <Drakan14@web.de>
2014-02-04 13:10:34 +0100

    removed unused imports

--- GrandmaApp/src/com/grandmaapp/notification/Notifications.java
+++ GrandmaApp/src/com/grandmaapp/notification/Notifications.java
@@ -2,14 +2,11 @@ package com.grandmaapp.notification;
 
 import com.example.grandmaapp.R;
 import com.grandmaapp.activities.GrandmaActivity;
-import android.app.Activity;
 import android.app.Notification;
-import android.app.Notification.InboxStyle;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
-import android.util.Log;
 
 public class Notifications
 {
